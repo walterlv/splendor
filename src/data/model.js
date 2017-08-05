2111fcf6f1571ac4b59a70627026bf2a6c399060
walterlv <xblero@gmail.com>
2017-08-05 21:45:56 +0800

    Add deck3 and artists.

--- src/data/model.js
+++ src/data/model.js
@@ -591,7 +591,271 @@ module.exports =
             },
         ],
         deck2: [
-
+            {
+                token: 'r',
+                points: 3,
+                price: {
+                    w: 3,
+                    b: 5,
+                    g: 3,
+                    k: 3,
+                },
+            },
+            {
+                token: 'k',
+                points: 5,
+                price: {
+                    r: 7,
+                    k: 3,
+                },
+            },
+            {
+                token: 'r',
+                points: 4,
+                price: {
+                    g: 7,
+                },
+            },
+            {
+                token: 'r',
+                points: 5,
+                price: {
+                    g: 7,
+                    r: 3,
+                },
+            },
+            {
+                token: 'g',
+                points: 4,
+                price: {
+                    b: 7,
+                },
+            },
+            {
+                token: 'g',
+                points: 3,
+                price: {
+                    w: 5,
+                    b: 3,
+                    r: 3,
+                    k: 3,
+                },
+            },
+            {
+                token: 'w',
+                points: 3,
+                price: {
+                    b: 5,
+                    g: 3,
+                    r: 5,
+                    k: 3,
+                },
+            },
+            {
+                token: 'b',
+                points: 4,
+                price: {
+                    w: 6,
+                    b: 3,
+                    k: 3,
+                },
+            },
+            {
+                token: 'w',
+                points: 5,
+                price: {
+                    w: 3,
+                    k: 7,
+                },
+            },
+            {
+                token: 'k',
+                points: 3,
+                price: {
+                    w: 3,
+                    b: 3,
+                    g: 5,
+                    r: 3,
+                },
+            },
+            {
+                token: 'w',
+                points: 4,
+                price: {
+                    w: 3,
+                    r: 3,
+                    k: 6,
+                },
+            },
+            {
+                token: 'g',
+                points: 4,
+                price: {
+                    w: 3,
+                    b: 6,
+                    g: 3,
+                },
+            },
+            {
+                token: 'w',
+                points: 4,
+                price: {
+                    k: 7,
+                },
+            },
+            {
+                token: 'r',
+                points: 4,
+                price: {
+                    b: 3,
+                    g: 6,
+                    r: 3,
+                },
+            },
+            {
+                token: 'b',
+                points: 5,
+                price: {
+                    w: 7,
+                    b: 3,
+                },
+            },
+            {
+                token: 'b',
+                points: 3,
+                price: {
+                    w: 3,
+                    g: 3,
+                    r: 3,
+                    k: 5,
+                },
+            },
+            {
+                token: 'k',
+                points: 4,
+                price: {
+                    g: 3,
+                    r: 6,
+                    k: 3,
+                },
+            },
+            {
+                token: 'b',
+                points: 4,
+                price: {
+                    w: 7,
+                },
+            },
+            {
+                token: 'k',
+                points: 4,
+                price: {
+                    r: 7,
+                },
+            },
+            {
+                token: 'g',
+                points: 5,
+                price: {
+                    b: 7,
+                    g: 3,
+                },
+            },
         ],
     },
+    artists: [
+        {
+            name: '红衣夫人',
+            points: 3,
+            price: {
+                r: 4,
+                g: 4,
+            },
+        },
+        {
+            name: '白胡子老头',
+            points: 3,
+            price: {
+                b: 4,
+                g: 4,
+            },
+        },
+        {
+            name: '短发勇士',
+            points: 3,
+            price: {
+                b: 4,
+                w: 4,
+            },
+        },
+        {
+            name: '胖乎乎的贵人',
+            points: 3,
+            price: {
+                k: 4,
+                r: 4,
+            },
+        },
+        {
+            name: '农妇',
+            points: 3,
+            price: {
+                g: 3,
+                b: 3,
+                w: 3,
+            },
+        },
+        {
+            name: '皇冠农妇',
+            points: 3,
+            price: {
+                k: 4,
+                w: 4,
+            },
+        },
+        {
+            name: '暗红衣夫人',
+            points: 3,
+            price: {
+                r: 4,
+                g: 4,
+            },
+        },
+        {
+            name: '红衣夫人',
+            points: 3,
+            price: {
+                k: 3,
+                b: 3,
+                w: 3,
+            },
+        },
+        {
+            name: '有钱的绅士',
+            points: 3,
+            price: {
+                g: 3,
+                b: 3,
+                r: 3,
+            },
+        },
+        {
+            name: '盔甲将军',
+            points: 3,
+            price: {
+                k: 3,
+                r: 3,
+                w: 3,
+            },
+        },
+        {
+            name: '天使',
+            points: 3,
+            price: {
+                k: 3,
+                r: 3,
+                g: 3,
+            },
+        },
+    ]
 };
