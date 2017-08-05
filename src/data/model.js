aab3f18c5c2645ca86d50ad6114865f1ea80c3e3
walterlv <xblero@gmail.com>
2017-08-05 21:29:11 +0800

    Add deck1 data.

--- src/data/model.js
+++ src/data/model.js
@@ -344,7 +344,251 @@ module.exports =
             },
         ],
         deck1: [
-
+            {
+                token: 'w',
+                points: 1,
+                price: {
+                    w: 2,
+                    b: 3,
+                    r: 3,
+                },
+            },
+            {
+                token: 'g',
+                points: 1,
+                price: {
+                    w: 2,
+                    b: 3,
+                    k: 2,
+                },
+            },
+            {
+                token: 'k',
+                points: 2,
+                price: {
+                    g: 5,
+                    r: 3,
+                },
+            },
+            {
+                token: 'r',
+                points: 1,
+                price: {
+                    b: 3,
+                    r: 2,
+                    k: 3,
+                },
+            },
+            {
+                token: 'g',
+                points: 2,
+                price: {
+                    w: 4,
+                    b: 2,
+                    k: 1,
+                },
+            },
+            {
+                token: 'b',
+                points: 2,
+                price: {
+                    w: 5,
+                    b: 3,
+                },
+            },
+            {
+                token: 'g',
+                points: 2,
+                price: {
+                    b: 5,
+                    g: 3,
+                },
+            },
+            {
+                token: 'k',
+                points: 2,
+                price: {
+                    b: 1,
+                    g: 4,
+                    r: 2,
+                },
+            },
+            {
+                token: 'w',
+                points: 1,
+                price: {
+                    g: 3,
+                    r: 2,
+                    k: 2,
+                },
+            },
+            {
+                token: 'g',
+                points: 1,
+                price: {
+                    w: 3,
+                    g: 2,
+                    r: 3,
+                },
+            },
+            {
+                token: 'r',
+                points: 2,
+                price: {
+                    k: 5,
+                },
+            },
+            {
+                token: 'b',
+                points: 3,
+                price: {
+                    b: 6,
+                },
+            },
+            {
+                token: 'b',
+                points: 2,
+                price: {
+                    w: 2,
+                    r: 1,
+                    k: 4,
+                },
+            },
+            {
+                token: 'k',
+                points: 1,
+                price: {
+                    w: 3,
+                    g: 3,
+                    k: 2,
+                },
+            },
+            {
+                token: 'r',
+                points: 2,
+                price: {
+                    w: 1,
+                    b: 4,
+                    g: 2,
+                },
+            },
+            {
+                token: 'w',
+                points: 2,
+                price: {
+                    g: 1,
+                    r: 4,
+                    k: 2,
+                },
+            },
+            {
+                token: 'k',
+                points: 2,
+                price: {
+                    w: 5,
+                },
+            },
+            {
+                token: 'k',
+                points: 1,
+                price: {
+                    w: 3,
+                    b: 2,
+                    g: 2,
+                },
+            },
+            {
+                token: 'k',
+                points: 3,
+                price: {
+                    k: 6,
+                },
+            },
+            {
+                token: 'r',
+                points: 1,
+                price: {
+                    w: 2,
+                    r: 2,
+                    k: 3,
+                },
+            },
+            {
+                token: 'b',
+                points: 1,
+                price: {
+                    b: 2,
+                    g: 2,
+                    r: 3,
+                },
+            },
+            {
+                token: 'r',
+                points: 2,
+                price: {
+                    w: 3,
+                    k: 5,
+                },
+            },
+            {
+                token: 'w',
+                points: 2,
+                price: {
+                    r: 5,
+                    k: 3,
+                },
+            },
+            {
+                token: 'b',
+                points: 1,
+                price: {
+                    b: 2,
+                    g: 3,
+                    k: 3,
+                },
+            },
+            {
+                token: 'b',
+                points: 2,
+                price: {
+                    b: 5,
+                },
+            },
+            {
+                token: 'g',
+                points: 2,
+                price: {
+                    g: 5,
+                },
+            },
+            {
+                token: 'w',
+                points: 3,
+                price: {
+                    w: 6,
+                },
+            },
+            {
+                token: 'r',
+                points: 3,
+                price: {
+                    r: 6,
+                },
+            },
+            {
+                token: 'g',
+                points: 3,
+                price: {
+                    g: 6,
+                },
+            },
+            {
+                token: 'w',
+                points: 2,
+                price: {
+                    r: 5,
+                },
+            },
         ],
         deck2: [
 
